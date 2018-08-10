76da3db42cd8a11041f4ac2673e934be144ce4f1
fridolph <249121486@qq.com>
2018-08-10 18:01:11 +0800

    api

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
-import {Provider} from 'react-redux'
-import store from './containers/Todo/redux/store'
+// import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 )
-registerServiceWorker()
+// registerServiceWorker()
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,20 +1,82 @@
-export function GET(url, data) {
-  return fetch(url, {
-    data
-  }).then(res => {
-    return res.json()
+import axios from 'axios'
+
+const service = axios.create({
+  baseURL: '/api',
+  timeout: 5000
+})
+
+service.interceptors.request.use(
+  config => {
+    return config
+  },
+  err => {
+    return Promise.reject(err)
+  }
+)
+
+service.interceptors.response.use(
+  resp => {
+    if (resp.status === 200) {
+      // console.log('resp', resp.data)
+      return resp.data
+    } else {
+      console.error('请求码错误')
+    }
+  },
+  err => {
+    return Promise.reject(err)
+  }
+)
+
+const GET = (url, params = {}) => {
+  return service({
+    url,
+    method: 'get',
+    data: params
   })
 }
 
-export function POST(url, data) {
-  return fetch(url, {
+const POST = (url, data) => {
+  return service({
+    url,
     method: 'post',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    withCredentials: true,
-    data
-  }).then(res => {
-    return res.json()
+    data,
+    transformRequest: [
+      function(data) {
+        let ret = []
+        for (let k in data) {
+          if (data[k]) {
+            ret.push(encodeURIComponent(k) + '=' + encodeURIComponent(data[k]))
+          }
+        }
+        return ret.join('&')
+      }
+    ]
   })
+}
+
+// export function GET(url, data) {
+//   return fetch(url, {
+//     data
+//   }).then(res => {
+//     return res.json()
+//   })
+// }
+
+// export function POST(url, data) {
+//   return fetch(url, {
+//     method: 'post',
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded'
+//     },
+//     withCredentials: true,
+//     data
+//   }).then(res => {
+//     return res.json()
+//   })
+// }
+
+export {
+  GET,
+  POST
 }
